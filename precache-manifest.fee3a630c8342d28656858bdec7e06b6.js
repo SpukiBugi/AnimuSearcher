a67d0d7a2e83c066506d73706428c60cad5c76b2
ssdesu <Jaguar757@hotmail.com>
2022-12-03 02:11:01 +0300

    b927be5

---
+++ precache-manifest.fee3a630c8342d28656858bdec7e06b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "389bca1babd8510d811f",
+    "url": "/AnimuSearcher/static/css/main.3a09d193.chunk.css"
+  },
+  {
+    "revision": "389bca1babd8510d811f",
+    "url": "/AnimuSearcher/static/js/main.a127ec3a.chunk.js"
+  },
+  {
+    "revision": "fbd6a748c35ce6f23f03",
+    "url": "/AnimuSearcher/static/js/runtime~main.aca1f628.js"
+  },
+  {
+    "revision": "6894aec02f10f16d890f",
+    "url": "/AnimuSearcher/static/js/2.795cd3bc.chunk.js"
+  },
+  {
+    "revision": "381172b43b7987146fa49c2096028b95",
+    "url": "/AnimuSearcher/index.html"
+  }
+];
